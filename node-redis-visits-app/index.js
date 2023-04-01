4570cf773173129af4b7c74485e2e65bb482c179
Vipin <vipinkumarcp4@gmail.com>
2023-04-01 23:28:51 +0530

    build docker compose

---
+++ node-redis-visits-app/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const redis = require('redis')
+
+
+const app = express();
+
+//redis client connector
+
+
+const client = redis.createClient({
+    host: 'redis-server',
+    port : 6379
+});
+client.set('visits',0);
+
+app.get('/',(req,res)=>{
+client.get('visits',(err,visits)=>{
+    res.send('Number of vists is'+visits);
+    client.set('visits',parseInt(visits) + 1)
+})
+
+})
+
+
+
+app.listen(8081,()=>{
+
+    console.log('listing on port 8081')
+})
